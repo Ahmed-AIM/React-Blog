02eb2f1b192ad4cd6d14d879ad765ea48fa32781
Vishwajeet Tyagi <Vishwajeet.tyagi.07@gmail.com>
2024-05-31 04:22:52 +0530

    First Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,127 @@
-import React from "react";
-import styled from "styled-components";
+import { Facebook, Instagram, LinkedIn, MailOutline, Phone, RoomOutlined, Twitter } from "@material-ui/icons";
+import styled from "styled-components"
+import { mobile } from "../responsive";
 
 const Container = styled.div`
-  height: 10%;
-  background-color: #111;
-  color: lightgray;
-`;
-
-const Wrapper = styled.div`
-  padding: 20px;
   display: flex;
-  justify-content: space-between;
-  @media only screen and (max-width: 480px) {
-    padding: 10px;
-  }
+  ${mobile({ flexDirection: "column" })}
+
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
 
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${props => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right:20px;
+`;
+
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
 const List = styled.ul`
-  padding: 0;
   margin: 0;
+  padding: 0;
   list-style: none;
   display: flex;
+  flex-wrap: wrap;
 `;
-
 const ListItem = styled.li`
-  margin-right: 20px;
-  @media only screen and (max-width: 480px) {
-    margin-right: 10px;
-    font-size: 14px;
-  }
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
 
 `;
-const Copyright = styled.span`
-  @media only screen and (max-width: 480px) {
-    font-size: 14px;
-  }
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 100%;
 `;
 
 const Footer = () => {
   return (
     <Container>
-      <Wrapper>
-        <List>
-          <ListItem>Guide</ListItem>
-          <ListItem>Support</ListItem>
-          <ListItem>API</ListItem>
-          <ListItem>Community</ListItem>
-        </List>
-        <Copyright>Lama Dev Ⓒ</Copyright>
-      </Wrapper>
-    </Container>
-  );
-};
+      <Left>
+        <Logo>ATHLETIC BRIJ</Logo>
+        <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ratione repellat quas delectus est ad qui, quasi enim nemo quia repellendus, cupiditate neque amet assumenda dolores sapiente dolore explicabo deserunt itaque! Suscipit, quod reiciendis!</Desc>
+        <SocialContainer>
+          <SocialIcon color="385999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <LinkedIn />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
 
-export default Footer;
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+
+        <Title>Contact</Title>
+        <ContactItem><RoomOutlined style={{ marginRight: "10px" }} /> 622 Dixie Path, South Tobinchester 98336
+        </ContactItem>
+        <ContactItem><Phone style={{ marginRight: "10px" }} />+1 234 56 78
+        </ContactItem>
+        <ContactItem><MailOutline style={{ marginRight: "10px" }} />athletic.brij@gmail.
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/cXFnLLV/3.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/DG69bQ4/2.png",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/cXFnLLV/3.png",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+        cat: "women"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+        cat: "coat"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+        cat: "jeans"
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/51tzcymjezL._SX679_.jpg",
+    },
+    {
+        id: 2,
+        img: "https://www.prada.com/content/dam/pradabkg_products/2/2VZ/2VZ135/2CX2F0594/2VZ135_2CX2_F0594_V_HYL_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg"
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 4,
+        img: "https://m.media-amazon.com/images/I/318icNIG-gL._SX300_SY300_QL70_FMwebp_.jpg",
+    },
+    {
+        id: 5,
+        img: "https://www.prada.com/content/dam/pradabkg_products/U/UJN/UJN911/15DAF0005/UJN911_15DA_F0005_S_OOO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg",
+    },
+    {
+        id: 6,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 7,
+        img: "https://m.media-amazon.com/images/I/51sJ+Z6GDaL._SX679_.jpg",
+    },
+    {
+        id: 8,
+        img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/a52dca73-a5d0-46e9-b1b3-468bb57463a5/phantom-luna-2-elite-lv8-fg-high-top-football-boot-Hvzq44.png",
+    },
+]
